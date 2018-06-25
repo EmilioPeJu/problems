3ef78c70523e1266e21842fa58eb2b957740a5ce
hir12111 <hir12111@gmail.com>
2018-06-25 14:17:06 +0100

    solve 10071

---
+++ backtohighschoolphysics/backtohighschoolphysics.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	while(true) {
+		int v, t;
+		cin  >> v >> t;
+		if (cin.eof()) break;
+		cout << (v*2*t) << endl;
+	}
+
+	return 0;
+}
