45d073aa740e5424b4a67191c77cfa5477d3a3a8
hir12111 <hir12111@gmail.com>
2018-10-27 22:31:13 +0100

    fix productrectangle

--- productrectanglecpp/productrectanglecpp.cpp
+++ productrectanglecpp/productrectanglecpp.cpp
@@ -1,16 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-typedef pair<int, int> ii;
-typedef tuple<int, int, int> iii;
-typedef vector<ii > vii;
-typedef vector<iii > viii;
+
 #define DIM 2001
 long long table[DIM][DIM];
 long long sum[DIM][DIM];
 long long tmp[DIM];
 long long A[DIM];
 long long B[DIM];
+
 int main() {
 	ios::sync_with_stdio(false);
 	int n, m;
@@ -24,7 +21,7 @@ int main() {
 			table[i][j] = A[i]*B[j];
 	long long max_sum = -1000000000000;
 	for(int jstart=0;jstart<m;jstart++) {
-		memset(tmp, 0, sizeof(tmp[0])*n);
+		memset(tmp, 0, sizeof(tmp[0])*m);
 		for(int jend=jstart;jend<m;jend++) {
 			for(int i=0;i<n;i++) 
 				tmp[i] += table[i][jend];
