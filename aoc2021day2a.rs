fc43557750809f327d6c8da3b5b30e93c3be8478
Emilio Perez <emilio.perez-juarez@diamond.ac.uk>
2021-12-03 13:20:39 +0000

    Add aoc2021 day 2

---
+++ aoc2021day2a.rs
@@ -0,0 +1,65 @@
+use std::collections::VecDeque;
+use std::io;
+
+struct Scan {
+    buffer: VecDeque<String>,
+}
+
+impl Scan {
+    fn new() -> Scan {
+        Scan {
+            buffer: VecDeque::new(),
+        }
+    }
+
+    fn next_line(&self) -> io::Result<String> {
+        let mut line = String::new();
+        match io::stdin().read_line(&mut line)? {
+            0 => Err(io::Error::new(io::ErrorKind::Other, "EOF")),
+            _ => Ok(line),
+        }
+    }
+
+    fn next<T: std::str::FromStr>(&mut self) -> io::Result<T> {
+        loop {
+            if let Some(token) = self.buffer.pop_front() {
+                break match token.parse::<T>() {
+                    Ok(x) => Ok(x),
+                    _ => Err(io::Error::new(io::ErrorKind::Other, "parsing is fucked")),
+                };
+            }
+            let line = self.next_line()?;
+            self.buffer = line.split_whitespace().map(String::from).collect();
+        }
+    }
+}
+
+fn _main() -> io::Result<()> {
+    let mut scan = Scan::new();
+    let mut x = 0isize;
+    let mut y = 0isize;
+    loop {
+        let command = match scan.next::<String>() {
+            Ok(val) => val,
+            Err(_) => break,
+        };
+        match &command[..] {
+            "forward" => x += scan.next::<isize>().unwrap(),
+            "down" => y += scan.next::<isize>().unwrap(),
+            "up" => y -= scan.next::<isize>().unwrap(),
+            _ => (),
+        }
+    }
+    println!("{}", x * y);
+    Ok(())
+}
+
+fn main() -> io::Result<()> {
+    std::thread::Builder::new()
+        .stack_size(1 << 23)
+        .spawn(_main)
+        .unwrap()
+        .join()
+        .unwrap()?;
+    Ok(())
+}
